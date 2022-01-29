02970d204388152d07e55a7c1f0cab0459260912
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-01-29 21:30:05 +0100

    Inplement AI Actions

---
+++ Assets/CORE/Scripts/AI/Actions/ActionMeeleAttack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "AI/Actions/Meele Attack", fileName = "MeeleAttack")]
+public class ActionMeeleAttack : AIAction
+{
+    public override void Act(StateController controller)
+    {
+        Attack(controller);
+    }
+
+    private void Attack(StateController controller)
+    {
+        // Stop
+        controller.CharacterMovement.SetHorizontal(0f);
+        controller.CharacterMovement.SetVertical(0f);
+        
+        // Attack
+        //controller.CharacterWeapon.CurrentWeapon.UseWeapon();
+    }
+}
