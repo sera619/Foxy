759847306acd618b9781d8fad95e22dda3e4cb31
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-02-05 22:52:18 +0100

    Create PlayerSpell.cs

---
+++ Assets/CORE/Scripts/Weapon/PlayerSpell.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpell : MonoBehaviour
+{
+    [Header("Name")]
+    [SerializeField] private string spellName = "";
+
+    [Header("Settings")]
+    [SerializeField] private float timeBtwCasts = 0.5f;
+    
+    [Header("Spell")]
+    [SerializeField] private bool useMagazine = true;
+    [ SerializeField] private int magazineSize = 30;
+    [SerializeField] private bool autoReload = true;
+
+    [Header("Effects")]
+    [SerializeField] private ParticleSystem spellEffect;
+
+    public string SpellName => spellName;
+    public Character SpellOwner { get; set; }
+    public int CurrentAmmo  { get; set; }
+    public SpellAmmo SpellAmmo { get; set;}
+
+    public int MagazineSize => magazineSize;
+    public bool UseMagazine => useMagazine;
+    public bool CanCast { get; set; }
+
+
+    private float nextCastTime;
+    private CharController controller;
+    private Animator animator;
+    private readonly int spellUseParameter = Animator.StringToHash("SpellUse");
+
+    protected virtual void Awake(){
+        SpellAmmo = GetComponent<SpellAmmo>();
+        //animator = GetComponent<Animator>();
+    }
+
+    protected virtual void Update(){
+        SpellCanShoot();
+    }
+
+
+    // Spell Logic
+    public virtual void UseSpell(){
+        StartCast();
+    }
+
+    private void StartCast(){
+        if(useMagazine){
+            if(SpellAmmo != null){
+                if (SpellAmmo.CanUseSpell()){
+                    RequestShot();            
+                }else{
+                    if(autoReload){
+                        Reload();
+                    }
+                }
+            }
+        }else{
+            RequestShot();
+        }
+    }
+
+
+    protected virtual void RequestShot(){
+        if(!CanCast){
+            return;
+        }
+        //animator.SetTrigger(spellUseParameter);
+        SpellAmmo.ConsumeAmmo();
+        //spellEffect.Play();
+    }
+
+
+    public void Reload(){
+        if (SpellAmmo != null){
+            if (useMagazine){
+                SpellAmmo.RefillAmmo();
+            }
+        }
+    }
+
+
+    protected virtual void SpellCanShoot(){
+        if(Time.time > nextCastTime){
+            CanCast = true;
+            nextCastTime = Time.time + timeBtwCasts;
+        }
+    }
+
+    public void SetOwner(Character owner){
+        SpellOwner = owner;
+        controller = SpellOwner.GetComponent<CharController>();
+    }
+
+
+
+
+
+
+
+}
+
