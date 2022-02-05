4ae4d56788f4822760650ce5a558cd918d1b01cb
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-02-05 22:52:48 +0100

    Create Projectile.cs

---
+++ Assets/CORE/Scripts/Weapon/SpellTypes/Projectile.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float speed = 60f;
+    [SerializeField] private float acceleration = 0f;
+
+    public Vector2 Direction { get; set; }
+    public float Speed { get; set; }
+    public Character ProjectileOwner { get; set; }
+    private Rigidbody2D myBody;
+    private Collider2D myCollider;
+    private SpriteRenderer spriteRenderer;
+    private Vector2 movement;
+    private bool canMove;
+
+    private void Awake() {
+        Speed = speed;
+        canMove = true;
+
+        myBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        myCollider = GetComponent<Collider2D>();
+    }
+
+    private void FixedUpdate() {
+        if(canMove){
+            MoveProjectile();
+        }
+    }
+
+    public void MoveProjectile(){
+        movement = Direction * (Speed / 10f)  * Time.fixedDeltaTime;
+        myBody.MovePosition(myBody.position + movement);
+        
+        Speed += acceleration * Time.deltaTime;
+    }
+
+    public void SetDirection(Vector2 newDirection, Quaternion rotation){
+        Direction = newDirection;
+        transform.rotation = rotation;
+    }
+
+    public void ResetProjectile(){
+        spriteRenderer.enabled = false;
+    }
+
+
+    public void DisableProjectile(){
+        canMove = false;
+        spriteRenderer.enabled = false;
+        myCollider.enabled = false;
+    }
+
+    public void EnableProjectile(){
+        canMove = true;
+        spriteRenderer.enabled = true;
+        myCollider.enabled = true;
+    }
+
+}
+
+
+
