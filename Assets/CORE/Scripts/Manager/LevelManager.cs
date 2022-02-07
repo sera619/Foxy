3224bac1dc5dfe7111dc0f7a5a4ac211a131fbeb
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-02-07 09:44:34 +0100

    adding TMP, GameObject , and levelname to LevelManager

--- Assets/CORE/Scripts/Manager/LevelManager.cs
+++ Assets/CORE/Scripts/Manager/LevelManager.cs
@@ -2,23 +2,39 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LevelManager : Singleton<LevelManager>
 {
     [SerializeField] private Character playableCharacter;
     [SerializeField] private Transform spawnPosition;
     [SerializeField] private GameObject transitionPlayer;
+    
+    [Header("Level Information")]
+    [SerializeField] private string levelName;
+    [SerializeField] private TextMeshProUGUI leveNameTMP;
+    [SerializeField] private GameObject levelInformationPanel;
 
+
+
+
+    private float activeTime = 4f;
+    private float _timeChecker;
+    private bool levelinfoShowing = false;
     private Animator transAnimator;
     public Transform Player { get; set; }
+    public string LevelName => levelName;
 
     private void Start(){
         transAnimator = transitionPlayer.GetComponent<Animator>();
         Player = playableCharacter.transform;
         Camera2D.Instance.Target = playableCharacter.transform;
+        _timeChecker = activeTime;
+        ShowLevelInformation();
     }
     
     private void Update(){
+        ShowInfo();
         if(Input.GetKeyDown(KeyCode.P)){
             ReviveCharacter();
         }
@@ -27,6 +43,29 @@ public class LevelManager : Singleton<LevelManager>
         if (playableCharacter.GetComponent<Health>().CurrentHealth <= 0){
             playableCharacter.GetComponent<Health>().Revive();
             playableCharacter.transform.position = spawnPosition.position;
+        }
+    }
+
+
+    private void ShowInfo(){
+        if(levelinfoShowing && _timeChecker > 0){
+            _timeChecker -= Time.deltaTime;
+
+        }else{
+            _timeChecker =  activeTime;
+            levelinfoShowing = false;
+            levelInformationPanel.SetActive(false);
+        }
+
+    }
+
+
+
+    private void ShowLevelInformation(){
+        if(levelName != "" && levelInformationPanel != null){
+            leveNameTMP.text = LevelName;
+            levelInformationPanel.SetActive(true);
+            levelinfoShowing = true;
         }
     }
 }
