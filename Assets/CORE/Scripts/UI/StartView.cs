6cfac0db2112f2e0d33c97b6697720c1aab91491
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-02-07 13:37:26 +0100

    Setup Get Playername Logic

--- Assets/CORE/Scripts/UI/StartView.cs
+++ Assets/CORE/Scripts/UI/StartView.cs
@@ -2,10 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
 
 public class StartView : MonoBehaviour
 {
+
+    [SerializeField] private GameObject nameGetPanel;
+    [SerializeField] private GameObject nameCheckPanel;
+    [SerializeField] private GameObject menuPanel;
+    [SerializeField] private GameObject namePanel;
+
+    [SerializeField] private TextMeshProUGUI checkNameTMP;
+    
+    [SerializeField] private TMP_InputField choosenName;
+
+    private string playerName;
+    
+
+
+
     public void StartGame(){
+        SaveLoad.Save<float>(0f, "PlayerCurrentExp");
+        SaveLoad.Save<float>(20f, "PlayerCurrentMaxExp");
+        SaveLoad.Save<int>(1, "PlayerLevel");
         SceneManager.LoadScene(3);
     }
 
@@ -13,6 +34,38 @@ public class StartView : MonoBehaviour
     public void EndGame(){
         Application.Quit();
     }
+
+
+    public void GoBack(){
+        menuPanel.SetActive(true);
+        nameGetPanel.SetActive(false);
+        nameCheckPanel.SetActive(false);
+        namePanel.SetActive(false);
+    }
+
+    public void GetName(){
+        namePanel.SetActive(true);
+        nameGetPanel.SetActive(true);
+        if(menuPanel.activeSelf){
+            menuPanel.SetActive(false);
+        }
+
+    }
+
+    public void CheckName(){
+        menuPanel.SetActive(false);
+        nameGetPanel.SetActive(false);
+        if(choosenName.text != ""){
+            playerName = choosenName.text;
+        }else{
+            playerName = "FoxyRox";
+        }
+        checkNameTMP.text ="Bis du sicher das dein Held\n\n" + playerName + "\n\nheissen soll?";
+        nameCheckPanel.SetActive(true);
+        SaveLoad.Save<string>(playerName, "PlayerName");
+        
+    }
+
 
 
 
