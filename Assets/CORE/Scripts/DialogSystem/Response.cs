837799313b30c20f96af571ffce8da1c0a89ce6a
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-02-10 19:42:07 +0100

    Create Response.cs

---
+++ Assets/CORE/Scripts/DialogSystem/Response.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Response
+{
+    [SerializeField] private string responseText;
+    [SerializeField] private DialogObject dialogObject;
+
+
+    public string ResponseText => responseText;
+    public DialogObject DialogObject => dialogObject;
+
+}
